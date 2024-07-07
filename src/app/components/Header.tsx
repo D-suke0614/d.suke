17a991852e647126567af135a6dc1bc5608b3667
Daisuke <d.suke.kida@gmail.com>
2024-07-07 18:13:12 +0900

    feat: ヘッダーに各ページへのリクを設置

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,22 @@
 const Header = () => {
   return (
-    <header className="py-12 mb-12 border-solid border-b border-[#484848]">
+    <header className="py-12 mb-12 flex items-center justify-between border-solid border-b border-[#484848]">
       <h1 className="text-6xl font-bold inline-block text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-700">
         <a href="/">d.suke</a>
       </h1>
+      <nav>
+        <ul className="flex items-center gap-12 text-xl font-bold text-indigo-400">
+          <li className="hover:text-indigo-300">
+            <a href="/about">About</a>
+          </li>
+          <li className="hover:text-indigo-300">
+            <a href="/work">Work</a>
+          </li>
+          <li className="hover:text-indigo-300">
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
