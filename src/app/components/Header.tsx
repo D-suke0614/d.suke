ca7ae4444aeb4a329444ddbd145a145f35ec41b9
Daisuke <d.suke.kida@gmail.com>
2024-07-07 18:15:22 +0900

    fix: Linkコンポーネントへ置き換え

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,21 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header className="py-12 mb-12 flex items-center justify-between border-solid border-b border-[#484848]">
       <h1 className="text-6xl font-bold inline-block text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-700">
-        <a href="/">d.suke</a>
+        <Link href="/">d.suke</Link>
       </h1>
       <nav>
         <ul className="flex items-center gap-12 text-xl font-bold text-indigo-400">
           <li className="hover:text-indigo-300">
-            <a href="/about">About</a>
+            <Link href="/about">About</Link>
           </li>
           <li className="hover:text-indigo-300">
-            <a href="/work">Work</a>
+            <Link href="/work">Work</Link>
           </li>
           <li className="hover:text-indigo-300">
-            <a href="/contact">Contact</a>
+            <Link href="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
